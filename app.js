9a5a59044b34cb77b0ac2e4abe03ba19419e2c64
couzts94 <couzts94@gmail.com>
2026-05-02 13:20:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+const supabase = window.supabase.createClient(
+  "https://haajilrpwlhixolmrxno.supabase.co/rest/v1/",
+  "sb_publishable_yCkfw5KNMyzkCGUaaZo1PA_jcQttJi_"
+);
 
+async function login(email, password) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+}
+async function addPost(text) {
+  const user = await supabase.auth.getUser();
+
+  await supabase.from("posts").insert([
+    {
+      content: text,
+      user_id: user.data.user.id
+    }
+  ]);
+}
+async function loadPosts() {
+  const { data } = await supabase
+    .from("posts")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  console.log(data);
+}
